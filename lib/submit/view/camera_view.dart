76810117a6581dca90f087c4d3dce66932ac146f
Pamela-code <pamela.souza-jpa@hotmail.com>
2022-09-04 21:14:27 -0300

    configurações de camera parte 2

--- lib/submit/view/camera_view.dart
+++ lib/submit/view/camera_view.dart
@@ -41,11 +41,50 @@ class _CameraPageState extends State<CameraPage> {
     final CameraController cameraController = CameraController(
       camera,
       ResolutionPreset.high,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
     );
+    controller = cameraController;
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      print(e.description);
+    }
+    if (mounted) {
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Foto'),
+        backgroundColor: Colors.grey[900],
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.grey[900],
+        child: Center(
+          child: _foto(),
+        ),
+      ),
+      floatingActionButton: (imagem != null)
+          ? FloatingActionButton.extended(
+              onPressed: () => Navigator.pop(context),
+              label: const Text("Finalizar"),
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  _foto() {
+    return Container(
+      width: size!.width - 50,
+      height: size!.height - (size!.height / 3),
+    );
   }
 }
