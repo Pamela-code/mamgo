9eda9a4280f20b070c74ada8a72adb4a17fdec38
Pamela-code <pamela.souza-jpa@hotmail.com>
2022-09-06 01:45:02 -0300

    tentativa de captura de imagem

--- lib/submit/view/camera_view.dart
+++ lib/submit/view/camera_view.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
@@ -14,7 +12,6 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late CameraController controller;
   XFile? imagem;
-  Size? size;
 
   @override
   void initState() {
@@ -39,41 +36,6 @@ class _CameraPageState extends State<CameraPage> {
     super.dispose();
   }
 
-  // _loadCameras() async {
-  //   try {
-  //     cameras = await availableCameras();
-  //     _startCamera();
-  //   } on CameraException catch (e) {
-  //     print('print 1 ${e.description}');
-  //   }
-  // }
-
-  // _startCamera() {
-  //   if (cameras.isEmpty) {
-  //     print('Camera não foi encontrada');
-  //   } else {
-  //     _previewCamera(cameras.first);
-  //   }
-  // }
-
-  // _previewCamera(CameraDescription camera) async {
-  //   final CameraController cameraController = CameraController(
-  //     camera,
-  //     ResolutionPreset.medium,
-  //     enableAudio: false,
-  //     imageFormatGroup: ImageFormatGroup.jpeg,
-  //   );
-  //   controller = cameraController;
-  //   try {
-  //     await cameraController.initialize();
-  //   } on CameraException catch (e) {
-  //     print('print 2 ${e.description}');
-  //   }
-  //   if (mounted) {
-  //     setState(() {});
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
     if (!controller.value.isInitialized) {
@@ -81,7 +43,7 @@ class _CameraPageState extends State<CameraPage> {
         child: CircularProgressIndicator(),
       );
     }
-    size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Foto'),
@@ -89,83 +51,34 @@ class _CameraPageState extends State<CameraPage> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: Container(
-        color: Colors.grey[900],
-        child: Center(
-          child: CameraPreview(controller),
-        ),
+      body: Column(
+        children: [
+          imagem == null
+              ? Column(
+                  children: [
+                    Container(
+                      color: Colors.grey[900],
+                      child: Center(
+                        child: CameraPreview(controller),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          imagem = await controller.takePicture();
+                          setState(() {});
+                        },
+                        child: const Text('Pronto'),
+                      ),
+                    ),
+                  ],
+                )
+              : Image.network(
+                  imagem!.path,
+                ),
+        ],
       ),
-      floatingActionButton: (imagem != null)
-          ? FloatingActionButton.extended(
-              onPressed: () => Navigator.pop(context),
-              label: const Text("Finalizar"),
-            )
-          : null,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-
-  // _foto() {
-  //   return Container(
-  //     width: size!.width - 50,
-  //     height: size!.height - (size!.height / 3),
-  //     child: imagem == null
-  //         ? _cameraPreviewWidget()
-  //         : Image.file(
-  //             File(imagem!.path),
-  //             fit: BoxFit.contain,
-  //           ),
-  //   );
-  // }
-
-  // _cameraPreviewWidget() {
-  //   final CameraController? cameraController = controller;
-
-  //   if (cameraController == null || cameraController.value.isInitialized) {
-  //     return const Center(
-  //       child: Text(
-  //         'Camera não disponível',
-  //         style: TextStyle(
-  //           color: Colors.white,
-  //         ),
-  //       ),
-  //     );
-  //   } else {
-  //     return Stack(
-  //       alignment: AlignmentDirectional.bottomCenter,
-  //       children: [
-  //         CameraPreview(controller!),
-  //         _botaoCaptura(),
-  //       ],
-  //     );
-  //   }
-  // }
-
-  // _botaoCaptura() {
-  //   return Padding(
-  //     padding: EdgeInsets.only(bottom: 24),
-  //     child: CircleAvatar(
-  //       radius: 32,
-  //       backgroundColor: Colors.green,
-  //       child: IconButton(
-  //         icon: Icon(Icons.camera_alt),
-  //         onPressed: tirarFoto,
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // tirarFoto() async {
-  //   final CameraController? cameraController = controller;
-  //   if (cameraController != null && cameraController.value.isInitialized) {
-  //     try {
-  //       XFile file = await cameraController.takePicture();
-  //       if (mounted) {
-  //         setState(() => imagem = file);
-  //       }
-  //     } on CameraException catch (e) {
-  //       print(e.description);
-  //     }
-  //   }
-  // }
 }
